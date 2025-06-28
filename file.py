e658694f436add6b9ff66eaeabc22b76b1d0ced9
Kevin <kevin.wartmann@students.fhnw.ch>
2025-06-28 18:05:23 +0200

    mathe file

---
+++ file.py
@@ -0,0 +1,37 @@
+def multi(x,y):
+    """
+    This function multiplies two numbers.
+
+    Parameters:
+    x (int or float): The first number.
+    y (int or float): The second number.
+
+    Returns:
+    int or float: The product of x and y.
+    """
+    return x * y
+
+def absolute(x):
+    """
+    This function returns the absolute value of a number.
+
+    Parameters:
+    x (int or float): The number to find the absolute value of.
+
+    Returns:
+    int or float: The absolute value of x.
+    """
+    return x if x >= 0 else -x
+
+def add(x, y):
+    """
+    This function adds two numbers.
+
+    Parameters:
+    x (int or float): The first number.
+    y (int or float): The second number.
+
+    Returns:
+    int or float: The sum of x and y.
+    """
+    return x + y
